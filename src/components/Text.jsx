4e895d9fd8371d21ff900ee5b611118fb39d5d65
amr <amrkhaledelsisy@gmail.com>
2026-05-05 03:44:25 +0300

    feat: add typing logic

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useRef, useEffect } from "react";
 
 function getRandomText(data, activeDiff) {
   const diffKey = activeDiff.toLowerCase().trim();
@@ -14,15 +14,36 @@ function getRandomText(data, activeDiff) {
 
 export default function Text({ data, activeDiff, activeMode }) {
   const [isStarted, setIsStarted] = useState(false);
-
+  const inputRef = useRef(null);
+  const [userInput, setUserInput] = useState("");
   const isReady = activeDiff.trim() !== "" && activeMode.trim() !== "";
 
   const currentDisplay = useMemo(() => {
     return getRandomText(data, activeDiff);
   }, [activeDiff]);
 
+  function handleStart() {
+    inputRef.current.focus();
+  }
+  useEffect(() => {
+    if (isStarted && inputRef.current) {
+      setUserInput("");
+      inputRef.current.value = "";
+      inputRef.current.focus();
+    }
+  }, [activeDiff]);
+
   return (
     <div className="text-display-container">
+      <input
+        ref={inputRef}
+        type="text"
+        style={{ opacity: 0, position: "absolute", zIndex: -1 }}
+        onChange={(e) => {
+          setUserInput(e.target.value);
+          console.log("User Input:", e.target.value);
+        }}
+      />
       <div className="text-card">
         {!isStarted && (
           <div className="text-overlay">
@@ -30,7 +51,10 @@ export default function Text({ data, activeDiff, activeMode }) {
               <button
                 className="start-button"
                 disabled={!isReady}
-                onClick={() => setIsStarted(true)}
+                onClick={() => {
+                  setIsStarted(true);
+                  handleStart();
+                }}
                 style={{
                   opacity: isReady ? 1 : 0.5,
                   cursor: isReady ? "pointer" : "not-allowed",
@@ -55,7 +79,24 @@ export default function Text({ data, activeDiff, activeMode }) {
             transition: "all 0.3s ease",
           }}
         >
-          {currentDisplay}
+          {currentDisplay.split("").map((char, index) => {
+            let color = "";
+            if (index < userInput.length) {
+              color = userInput[index] === char ? "#2ecc71" : "#e74c3c";
+            }
+
+            return (
+              <span
+                key={index}
+                style={{
+                  color: color,
+                  marginRight: char === " " ? "0.25ch" : "0",
+                }}
+              >
+                {char}
+              </span>
+            );
+          })}
         </p>
       </div>
     </div>
